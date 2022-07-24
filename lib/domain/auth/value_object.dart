3c0f5718224ad4c23056befa9560d4414b37b47d
patarchims <patarchims@gmail.com>
2022-07-24 11:02:42 +0700

    set up project

---
+++ lib/domain/auth/value_object.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_architecture_flutter/domain/core/failures.dart';
+import 'package:ddd_architecture_flutter/domain/core/value_objects.dart';
+import 'package:ddd_architecture_flutter/domain/core/value_validators.dart';
+
+class UserName extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory UserName(String input) {
+    return UserName._(
+      validateUserName(input),
+    );
+  }
+
+  const UserName._(this.value);
+}
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
